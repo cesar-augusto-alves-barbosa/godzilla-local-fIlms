3861449db2665635be45bb5ca1df960ca222b3a0
dev-cesar <cesar.barbosa@bandtec.com.br>
2022-04-07 14:25:32 -0300

    Feat: creating project

---
+++ API/api/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_FILMES (titulo, diretor, estoque) VALUES
+  ('Godzilla 200', 'Takao Okawara', 1),
+  ('"Godzilla Contra Ataca', 'Motoyoshi Oda', 5);
---
+++ API/api/src/test/java/com/gozilla/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gozilla.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
