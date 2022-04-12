187d59c03c5b22ab67860134a9da13c714d7f4c2
dev-cesar <cesar.barbosa@bandtec.com.br>
2022-04-12 18:52:06 -0300

    Add endpoint to return film to rental

--- API/api/src/main/java/com/gozilla/api/controller/AlugadorController.java
+++ API/api/src/main/java/com/gozilla/api/controller/AlugadorController.java
@@ -46,39 +46,56 @@ public class AlugadorController {
     public ResponseEntity alugarFilme(@PathVariable Integer idFilme) {
         Optional<Filme> filmeOptional = filmesRepository.findById(idFilme);
         if(filmeOptional.isPresent()) {
-            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Object usuarioLogado = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             String emailUsuario;
-            if (principal instanceof DetalheUsuario) {
-                emailUsuario = ((DetalheUsuario)principal).getUsername();
-            } else {
-                emailUsuario = principal.toString();
-            }
+            Boolean usuarioAutenticado = usuarioLogado instanceof DetalheUsuario;
+            emailUsuario = usuarioAutenticado ? ((DetalheUsuario) usuarioLogado).getUsername() : usuarioLogado.toString();
             Optional<Usuario> usuarioOptional = usuarioRepository.findUsuarioByEmail(emailUsuario);
-
             if(!usuarioOptional.isPresent()) {
                 return ResponseEntity.status(401).build();
-            }
-            Optional<Alugador> usuarioAlugador = repository.findByUsuarioId(usuarioOptional.get().getId());
-            if(usuarioAlugador.isPresent()) {
+            } else {
+                Optional<Alugador> usuarioAlugador = repository.findByUsuarioId(usuarioOptional.get().getId());
+                if(!usuarioAlugador.isPresent()) {
+                    Usuario usuario = usuarioOptional.get();
+                    Filme filme = filmeOptional.get();
+                    filme.setEstoque(filme.getEstoque() - 1);
+                    Alugador alugador = new Alugador();
+                    alugador.setFilme(filme);
+                    alugador.setUsuario(usuario);
+                    alugador.setDataAluguel(LocalDate.now());
+                    filmesRepository.save(filme);
+                    repository.save(alugador);
+                    return ResponseEntity.status(201).build();
+                }
                 return ResponseEntity.status(403).body("Você já possui um filme alugado, " +
                         "devolva seu filme para alugar outro.");
             }
-            Usuario usuario = usuarioOptional.get();
-            Filme filme = filmeOptional.get();
-            filme.setEstoque(filme.getEstoque() - 1);
-            Alugador alugador = new Alugador();
-            alugador.setFilme(filme);
-            alugador.setUsuario(usuario);
-            alugador.setDataAluguel(LocalDate.now());
-            filmesRepository.save(filme);
-            repository.save(alugador);
-            return ResponseEntity.status(201).build();
         }
         return ResponseEntity.status(401).build();
     }
 
     @DeleteMapping("/devolver/")
     public ResponseEntity devolverFilme() {
-        return ResponseEntity.status(404).build();
+        Object usuarioLogado = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String emailUsuario;
+        Boolean usuarioAutenticado = usuarioLogado instanceof DetalheUsuario;
+        emailUsuario = usuarioAutenticado ? ((DetalheUsuario) usuarioLogado).getUsername() : usuarioLogado.toString();
+        if(emailUsuario == null) {
+            return ResponseEntity.status(403).build();
+        }
+        Optional<Usuario> usuarioOptional = usuarioRepository.findUsuarioByEmail(emailUsuario);
+        Optional<Alugador> usuarioAlugadorOptional = repository.findByUsuarioId(usuarioOptional.get().getId());
+        if(!usuarioOptional.isPresent()) {
+            return ResponseEntity.status(401).build();
+        }
+        if(!usuarioAlugadorOptional.isPresent()) {
+            return ResponseEntity.status(204).build();
+        }
+        Alugador usuarioAlugador = usuarioAlugadorOptional.get();
+        Filme filmeAlugado = filmesRepository.findById(usuarioAlugador.getFilme().getId()).get();
+        filmeAlugado.setEstoque(filmeAlugado.getEstoque() + 1);
+        filmesRepository.save(filmeAlugado);
+        repository.delete(usuarioAlugador);
+        return ResponseEntity.status(200).build();
     }
 }
